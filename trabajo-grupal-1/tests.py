4d8fb5e8d8fd81c679292ad313b3149d4a589e47
felo1 <felipe.bazignan@gmail.com>
2023-05-05 12:38:54 -0400

    eliminado clase venta, clase compra avanzada

--- trabajo-grupal-1/tests.py
+++ trabajo-grupal-1/tests.py
@@ -1,18 +1,26 @@
 import main
 from main import *
 
+#cliente1 = Cliente("id1", "Ignacio", "Fuentealba", "correo@gmail.com", "25-enero", 25000000)
+#producto1 = Producto("001", "Producto 1", "Menaje", proveedor1, 100, 19990)
+
 clientes.append(cliente1)
 clientes.append(cliente2)
 clientes.append(cliente3)
 clientes.append(cliente4)
 clientes.append(cliente5)
 
-print(f"Saldo de {cliente1.nombre} es {cliente1.get_saldo()}")
-cliente1.set_saldo(-500)
-print(f"Saldo de {cliente1.nombre} es {cliente1.get_saldo()}")
-#producto1.stock(-500)
-#producto3.stock(50)
+print(f"Saldo de {cliente1.nombre} es {cliente1.saldo}")
+cliente1.saldo -= 5
+print(f"Saldo de {cliente1.nombre} es {cliente1.saldo}")
+print("==================================================")
 print(f"stock de {producto1.nombre} es {producto1.stock}")
 producto1.stock -= 500
 #producto1.stock = producto1.stock - 500
+print("==================================================")
 print(f"stock de {producto1.nombre} es {producto1.stock}")
+print("=========================pruebas de transacciones=========================")
+compra1 = Compra(cliente1, producto1, vendedor1, 10)#para probar desbordado de stock
+compra1 = Compra(cliente1, producto1, vendedor1, 4)
+
+compra1.procesar_compra()
