e1eaa1582136ce271d0b13e0f320d2e809a538fd
Felo <felipe.bazignan@gmail.com>
2023-06-13 22:22:02 -0400

    avances grupales 13-06

--- modulo-6/telovendo/telovendo/account/urls.py
+++ modulo-6/telovendo/telovendo/account/urls.py
@@ -3,6 +3,7 @@ from . import views # para que más abajo se pueda usar views.index
 urlpatterns = [
     path("", views.index, name = "index"),
     path("pg2", views.pg2, name = "pg2"),
+    path("inscripcion", views.crear_proveedor, name = "inscripcion")
     #path("usuarios", views.usuarios, name = "usuarios")
     # "" es lo que sigue de la url principal, en este caso es solo /, 
     # el segundo parámetro es lo que se debe renderizar al ingresar allí.
